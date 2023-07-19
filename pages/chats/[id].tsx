d703eacb506dfa57bde13df74d7d63d12359e955
LAPTOP-SSV2GS54\qortl <qortlgus100@gmail.com>
2023-07-19 13:06:58 +0900

    prisma, pscale 사용해 보기

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Layout from "../../components/layout";
+import Message from "../../components/message";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <Layout canGoBack title="Steve">
+      <div className="px-4 py-10 pb-16 space-y-4">
+        <Message message="Hi how much are you selling them for?" />
+        <Message message="I want ￦20,000" reversed />
+        <Message message="미쳤어" />
+        <form className="fixed inset-x-0 bottom-0 py-2 bg-white">
+          <div className="relative flex items-center w-full max-w-md mx-auto">
+            <input
+              type="text"
+              className="w-full pr-12 border-gray-300 rounded-full shadow-sm focus:ring-orange-500 focus:outline-none focus:border-orange-500"
+            />
+            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+              <button className="flex items-center px-3 text-sm text-white bg-orange-500 rounded-full focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 hover:bg-orange-600">
+                &rarr;
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default ChatDetail;
